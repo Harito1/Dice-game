fad95516534405f59a8a2809b9bca14cb53dfa1d
Harito1 <haritomafuya@gmail.com>
2023-06-23 13:12:01 +0200

    score tracking and holding functions updated

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 
 var scores, roundScores, activePlayer;
 
-scores = [0, 0];
-roundScores = 0;
-activePlayer = 0;
- 
-document.querySelector('.dice').style.display= 'none';
+ initialize()
 
-document.getElementById('score--0').textContent = '0';
-document.getElementById('score--1').textContent = '0';
-document.getElementById('current--0').textContent = '0';
-document.getElementById('current--1').textContent = '0';
+ // New game button.
+ document.querySelector('.btn--new').addEventListener('click', initialize)
 
+ // Role dice button.
 document.querySelector('.btn--roll').addEventListener('click', function() {
     
     // dice takes any random number between 1 and 6 after clicking
@@ -31,12 +26,56 @@ document.querySelector('.btn--roll').addEventListener('click', function() {
         document.querySelector('#current--' + activePlayer).textContent = roundScores;
 
     }else{
-        //next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        nextPlayer()
     }
+
+})
+
+/**
+ * this function is used to switch between players if the dice reaches 1 on the activePlayer
+ */
+function nextPlayer() {
+    
+    //next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScores = 0;
+
+    document.getElementById('current--0').textContent = '0' ;
+    document.getElementById('current--1').textContent = '0' ;
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn--hold').addEventListener('click', function() {
+    
+    // add currnet score to global score
+
+    scores[activePlayer] += roundScores;
+
+    // update the users interface
+
+    document.querySelector('#score--' + activePlayer).innerHTML = scores[activePlayer]
+
+    // check if player won the game
 
 })
 
 
 
 
+/**
+ * things that should happen before playing the game.
+ */
+function initialize() {
+    
+    scores = [0, 0];
+    roundScores = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display= 'none';
+
+    document.getElementById('score--0').textContent = '0';
+    document.getElementById('score--1').textContent = '0';
+    document.getElementById('current--0').textContent = '0';
+    document.getElementById('current--1').textContent = '0';
+}
