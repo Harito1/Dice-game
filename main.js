6e55f29905dc7c75e190588bac47a4f7df2657ea
Harito1 <haritomafuya@gmail.com>
2023-06-23 15:07:43 +0200

    dice game done

--- main.js
+++ main.js
@@ -90,4 +90,10 @@ function initialize() {
     document.getElementById('score--1').textContent = '0';
     document.getElementById('current--0').textContent = '0';
     document.getElementById('current--1').textContent = '0';
+
+    document.getElementById('name--0').textContent = 'Player 1';
+    document.getElementById('name--1').textContent = 'Player 2';
+
+    document.querySelector('.player--0').classList.remove('winner!')
+    document.querySelector('.player--1').classList.remove('winner!')
 }
